76c76fd5eac35934fdab9c51b800fe321c745a71
root <root@beaglebone.localdomain>
2015-06-09 13:11:57 +0000

    color-tracker: land something that works
    flicker on add, not sure what's going on

--- color-tracker.py
+++ color-tracker.py
@@ -15,37 +15,38 @@ class ColorTracker:
 
     def run(self):
         while True:
-            img = cv.QueryFrame(self.capture)
+            im = cv.QueryFrame(self.capture)
+            img = cv.CloneImage(im)
 
             #blur the source image to reduce color noise 
-            cv.Smooth(img, img, cv.CV_BLUR, 3);
+            cv.Smooth(img, img, cv.CV_BLUR, 12);
 
             #convert the image to hsv(Hue, Saturation, Value) so its  
             #easier to determine the color to track(hue) 
-            hsv_img = cv.CreateImage(cv.GetSize(img), 8, 3)
-            cv.CvtColor(img, hsv_img, cv.CV_BGR2HSV)
+            hsv = cv.CreateImage(cv.GetSize(img), 8, 3)
+            cv.CvtColor(img, hsv, cv.CV_BGR2HSV)
 
             #limit all pixels that don't match our criteria, in this case we are  
             #looking for purple but if you want you can adjust the first value in  
             #both turples which is the hue range(120,140).  OpenCV uses 0-180 as  
             #a hue range for the HSV color model 
-            thresholded_img =  cv.CreateImage(cv.GetSize(hsv_img), 8, 1)
+            threshold =  cv.CreateImage(cv.GetSize(hsv), 8, 1)
             ORANGE_MIN = cv.Scalar(20, 100, 100)
             ORANGE_MAX = cv.Scalar(30, 255, 255)
-            cv.InRangeS(hsv_img, ORANGE_MIN, ORANGE_MAX, thresholded_img)
+            cv.InRangeS(hsv, ORANGE_MIN, ORANGE_MAX, threshold)
 
             #determine the objects moments and check that the area is large  
             #enough to be our object 
             #thresholded_img = cv.GetMat(thresholded_img)
-            moments = cv.Moments(thresholded_img, 0)
-            area = int(cv.GetCentralMoment(moments, 0, 0))
+            moments = cv.Moments(cv.GetMat(threshold), 0)
+            area = cv.GetCentralMoment(moments, 0, 0)
 
             #there can be noise in the video so ignore objects with small areas 
-            if(area > 100000):
+            if area > 100000:
                 #determine the x and y coordinates of the center of the object 
                 #we are tracking by dividing the 1, 0 and 0, 1 moments by the area 
-                x = int(cv.GetSpatialMoment(moments, 1, 0)/area)
-                y = int(cv.GetSpatialMoment(moments, 0, 1)/area)
+                x = cv.GetSpatialMoment(moments, 1, 0)/area
+                y = cv.GetSpatialMoment(moments, 0, 1)/area
                 print 'x',x,'y',y,'area',area
                 
                 #create an overlay to mark the center of the tracked object 
@@ -59,7 +60,7 @@ class ColorTracker:
             #display the image  
             cv.ShowImage(color_tracker_window, img)
 
-            if cv.WaitKey(10) == 27:
+            if cv.WaitKey(100) == 27:
                 break
 
 if __name__=="__main__":
